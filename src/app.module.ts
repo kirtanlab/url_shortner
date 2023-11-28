701c008bcc43d18faefce207411005b6ed66ce2c
kirtanlab <kirtanprajapati0804@gmail.com>
2023-11-29 02:07:59 +0530

    uploaded

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configValidationSchema } from './config.schema';
+import { LinksModule } from './links/links.module';
+import { WildcardModule } from './wildcard/wildcard.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`stage.${process.env.STAGE}.env`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+    LinksModule,
+    WildcardModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
